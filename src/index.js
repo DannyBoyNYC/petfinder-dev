b0e747c71202c2b6e36a6c1401fdbcaa8f8cd703
Daniel Deverell <daniel.deverell@gmail.com>
2021-10-27 08:41:06 -0600

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
-// import staticPets from './pets';
-// import Pet from './Pet';
 import "./index.css";
 
 const App = () => {
-  // const [pets, setPets] = useState(staticPets);
-
   return (
     <main>
       <h1>Adopt-a-Pet</h1>
       <ul>
         <li>pets go here</li>
-        {/* {pets.map(pet => (
-          <li key={pet.id}>
-            <Pet pet={pet} />
-          </li>
-        ))} */}
       </ul>
       <button>Add a Pet</button>
     </main>
